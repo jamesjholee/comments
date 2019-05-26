59e6d044f46941f3bce35d4d464240cb07a72715
James <james.lee4789@gmail.com>
2019-05-26 17:49:54 -0400

    new component within a card component to take a child

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 5:50PM" avatarImg={faker.image.avatar()} content="react is cool!"/>
-      <CommentDetail author="James" timeAgo="Today at 12:00PM" avatarImg={faker.image.avatar()} content="DAMN"/>
-      <CommentDetail author="Name" timeAgo="Yesterday at 9:00AM" avatarImg={faker.image.avatar()} content="....."/>
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 5:50PM" avatarImg={faker.image.avatar()} content="react is cool!"/>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="James" timeAgo="Today at 12:00PM" avatarImg={faker.image.avatar()} content="DAMN"/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Name" timeAgo="Yesterday at 9:00AM" avatarImg={faker.image.avatar()} content="....."/>
+      </ApprovalCard>
     </div>
   );
 };
