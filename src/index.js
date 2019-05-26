0156f82f83b10aa7d4462c0395c6fcda34bfe2df
James <james.lee4789@gmail.com>
2019-05-26 17:30:53 -0400

    add author props to use with new comp

--- src/index.js
+++ src/index.js
@@ -6,23 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            James
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-
-          <div className="text">Nice This is the first comment!</div>
-        </div>
-      </div>
+      <CommentDetail author="Sam"/>
+      <CommentDetail author="James"/>
+      <CommentDetail author="Name"/>
     </div>
-
   );
 };
 
