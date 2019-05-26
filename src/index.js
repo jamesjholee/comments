60427568ac47183dc904c2559237e0648d76854f
James <james.lee4789@gmail.com>
2019-05-26 17:35:32 -0400

    add img content and time ago

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="James"/>
-      <CommentDetail author="Name"/>
+      <CommentDetail author="Sam" timeAgo="Today at 5:50PM" avatarImg={faker.image.avatar()} content="react is cool!"/>
+      <CommentDetail author="James" timeAgo="Today at 12:00PM" avatarImg={faker.image.avatar()} content="DAMN"/>
+      <CommentDetail author="Name" timeAgo="Yesterday at 9:00AM" avatarImg={faker.image.avatar()} content="....."/>
     </div>
   );
 };
-
 ReactDOM.render(<App />, document.querySelector('#root'));
